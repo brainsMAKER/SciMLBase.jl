451d26876f2b33a6946e9772499033868fe03614
Arthur Amorim <48725375+brainsMAKER@users.noreply.github.com>
2022-06-23 15:56:18 +0200

    fix first batch test -> solution is now a number

--- test/downstream/ensemble_first_batch.jl
+++ test/downstream/ensemble_first_batch.jl
@@ -1,10 +1,10 @@
 using OrdinaryDiffEq, Test, Statistics
 
-# tests for https://github.com/SciML/SciMLBase.jl/issues/190
+# test for https://github.com/SciML/SciMLBase.jl/issues/190
 prob = ODEProblem((u,p,t)->1.01u,0.5,(0.0,1.0))
 prob_func(prob,i,repeat) = remake(prob,u0=rand()*prob.u0)
 output_func(sol,i) = (last(sol),false)
-reduction(u,batch,I) = ([mean(u),var(u)], false)
+reduction(u,batch,I) = (append!(u,mean(batch)), false)
 # make sure first batch is timed (test using 1 batch but reduction)
 ensemble_prob = EnsembleProblem(prob,prob_func=prob_func,output_func=output_func,reduction=reduction)
 sim = solve(ensemble_prob,Tsit5(),EnsembleThreads(),trajectories=1000,batch_size=1000)
